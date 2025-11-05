74129c4edee79659b7a64b037a69440f7eb9d69a
Professional <pixellv3@gmail.com>
2025-11-05 17:32:50 +0300

    1

---
+++ main.py
@@ -0,0 +1,14 @@
+# Импортируем Flask
+from flask import Flask
+
+# Создаём экземпляр приложения Flask
+app = Flask(__name__)
+
+# Определяем маршрут для главной страницы
+@app.route("/")
+def home():
+    return "Привет, мир! Flask работает!"  # Возвращаем текст в браузер
+
+# Запускаем сервер
+if __name__ == "__main__":
+    app.run(debug=False)
